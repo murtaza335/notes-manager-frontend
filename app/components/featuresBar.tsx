a42a825618ae9fb09968e30b40c52214c0b5365f
murtaza_307 <murtazarasheedblogger@gmail.com>
2025-06-27 11:18:34 +0500

    yo

--- app/components/featuresBar.tsx
+++ app/components/featuresBar.tsx
@@ -19,7 +19,7 @@ export default function FeaturesBar({ className, role }: FeaturesBarProps) {
       <div className={`featuresBar flex justify-between items-center bg-[#1b1b1b] p-4 rounded-xl shadow-md mb-6 ${className}`}>
         <SearchBar className="p-2 bg-[#3a3a3a] text-white rounded-md focus:outline-none focus:ring-2 focus:ring-[#34b7ff] placeholder:text-[#929292]" />
         
-        <FilterButton className="ml-auto" role={role} />
+        <FilterButton className="ml-auto mr-10" role={role} />
         
         {role === 'employee' && <button
           onClick={() => setIsAddNoteOpen(true)}
